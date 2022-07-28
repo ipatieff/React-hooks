f83991cabebcc078261219282f04c55dd93a5052
Serge Ipatieff <ipatieff@yandex.ru>
2022-07-28 16:10:39 +0300

    add useThrottle

---
+++ useThrottle.ts
@@ -0,0 +1,20 @@
+import { useRef } from 'react';
+
+function useThrottle(func: VoidFunction, delay: number) {
+  const savedTimeout = useRef<ReturnType<typeof setTimeout> | null>();
+  const throttledFunc = function () {
+    if (savedTimeout.current) {
+      clearTimeout(savedTimeout.current);
+    }
+    const newTimeout = setTimeout(() => {
+      func();
+      if (newTimeout === savedTimeout.current) {
+        savedTimeout.current = null;
+      }
+    }, delay);
+    savedTimeout.current = newTimeout;
+  };
+  return throttledFunc;
+}
+
+export default useThrottle;

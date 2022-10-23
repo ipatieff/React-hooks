9f2f9a603589596a598e221c7a6844235a52211b
Serge Ipatieff <websystemansk@gmail.com>
2022-10-23 10:46:52 +0300

    upd trtl

--- useThrottle.ts
+++ useThrottle.ts
@@ -1,20 +1,19 @@
-import { useRef } from 'react';
+import {useRef} from 'react';
 
 function useThrottle(func: VoidFunction, delay: number) {
-  const savedTimeout = useRef<ReturnType<typeof setTimeout> | null>();
-  const throttledFunc = function () {
-    if (savedTimeout.current) {
-      clearTimeout(savedTimeout.current);
-    }
-    const newTimeout = setTimeout(() => {
-      func();
-      if (newTimeout === savedTimeout.current) {
-        savedTimeout.current = null;
-      }
-    }, delay);
-    savedTimeout.current = newTimeout;
-  };
-  return throttledFunc;
+    const savedTimeout = useRef<ReturnType<typeof setTimeout> | null>();
+    return function () {
+        if (savedTimeout.current) {
+            clearTimeout(savedTimeout.current);
+        }
+        const newTimeout = setTimeout(() => {
+            func();
+            if (newTimeout === savedTimeout.current) {
+                savedTimeout.current = null;
+            }
+        }, delay);
+        savedTimeout.current = newTimeout;
+    };
 }
 
 export default useThrottle;
@@ -25,4 +24,3 @@ export default useThrottle;
  * const handleClick = useThrottle(() => console.log('Do click'), 1500);
  *
  * **/
-
